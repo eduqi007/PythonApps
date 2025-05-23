d77c976fb6befc5ade43897c4eb66eb320dde93c
eduqi007 <89520521+eduqi007@users.noreply.github.com>
2025-05-23 13:08:59 -0400

    Update state.py

--- Simple Calculator/SimpleCalculatorV3/state.py
+++ Simple Calculator/SimpleCalculatorV3/state.py
@@ -1,13 +1,12 @@
 class CalculatorState:
-    def __init__(self):
-        self.f_num = 0   
-        self.f_ans = 0
+    def __init__(self): #The first state of the calculator
         self.sign_fix = 1
+        self.f_num = 0
+        self.f_ans = 0
         self.mult_or_sum_1 = 0
         self.mult_or_sum_2 = 1
         self.mult_or_div = 1
-
-    def reset(self):
+    def reset(self): #Its useful for reseting/clearing the calculator
         self.__init__()
 
 
